4ef70c671e7e6ac40f7f0c9f96c3dab280bf34e3
IamSynthetiC81 <marioskritikakis@gmail.com>
2026-03-02 13:42:51 +0200

    Sync participant release from private repo

--- lib/src/ErrorRegister.c
+++
@@ -1,9 +0,0 @@
-#include "ErrorRegister.h"
-
-/**
- * @brief Global PROGRAM_ERRORS register instance
- */
-ErrorRegister_t PROGRAM_ERRORS = {
-    .peripheralName = "PROGRAM",
-    .errorReg = 0
-};
